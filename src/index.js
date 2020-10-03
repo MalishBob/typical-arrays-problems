b3b90d66857ce4d50bad0fb22029634182cf2e3b
Azad <azad_63_mamedov@mail.ru>
2020-10-03 19:42:37 +0400

    feat: solution array task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if(typeof(array)===null || typeof(array)==='undefined' || array.length === 0) return 0;
+
+  let min_val;
+  for(let i = 0; i<array.length; i++){
+    if(i == 0) min_val = array[i];
+    if(min_val > array[i]) min_val = array[i];
+  }
+
+  return min_val;
+ 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof(array)===null || typeof(array)==='undefined' || array.length === 0) return 0;
+
+  let max_val;
+  for(let i = 0; i<array.length; i++){
+    if(i == 0) max_val = array[i];
+    if(max_val < array[i]) max_val = array[i];
+  }
+  return max_val;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof(array)===null || typeof(array)==='undefined' || array.length === 0) return 0;
+
+  let avg_val;
+  let summ = 0;
+  for(let i = 0; i<array.length; i++){
+    summ = summ + array[i];
+  }
+  avg_val = summ/array.length;
+  return avg_val;
+
 }
